bf631553834e1bc1a9f71cbb3bf23066e282289e
ajquidasol615 <ajquidasol615@gmail.com>
2023-03-28 11:15:00 +0800

    modified:   .next/cache/webpack/client-development/1.pack 	modified:   .next/cache/webpack/client-development/2.pack 	modified:   .next/cache/webpack/client-development/3.pack 	modified:   .next/cache/webpack/client-development/4.pack 	modified:   .next/cache/webpack/client-development/5.pack

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,13 +1,25 @@
-import MeetupItem from '../../components/meetups/MeetupItem';
-import { useRouter } from 'next/router';
+import MeetupDetails from '../../components/meetups/MeetupDetails';
+
+const imageLink =
+  'https://media.timeout.com/images/105387012/750/422/image.jpg';
+const data = {
+  id: 'm1',
+  title: 'A First Meetup',
+  image: imageLink,
+  address: '35 National Highway, Quezon, San Isidro, Isabela',
+  description: 'This a first meetup',
+};
+
 const MeetupDetailsPage = () => {
-  const router = useRouter();
-  const id = router.query.meetupId;
-  console.log(id)
   return (
     <>
       <h1>Meetup Details Page</h1>
-      <MeetupItem meetupId={id} />
+      <MeetupDetails
+        image={data.image}
+        title={data.title}
+        address={data.address}
+        description={data.description}
+      />
     </>
   );
 };
